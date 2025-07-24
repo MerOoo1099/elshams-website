a4b5dc45b8684abb2f4e71967b2bc8353b5339f7
Mer0oo1099 <amir.hany1011@gmail.com>
2025-07-24 19:39:10 +0300

    - edit some styles

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,8 @@
     id="home"
   >
     <div class="container relative z-1">
-      <div class="grid grid-cols-1 mt-12 w-1/2">
+      <!-- center this div in small screens -->
+      <div class="flex flex-col justify-center md:text-start text-center md:mt-12 mt-52 lg:w-1/2 md:w-4/6 w-full">
         <h4
           class="text-white lg:text-5xl text-3xl leading-normal font-medium mb-7 position-relative animate-slideInFromLeft"
         >
